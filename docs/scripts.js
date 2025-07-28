29543ba2ebe0a1ffd6810a898f25a0c653ae9d78
Guedes-hub <guedes.00.digital@gmail.com>
2025-07-28 20:41:58 -0300

    Volta ao inicio kkk o que estava padrão

--- docs/scripts.js
+++ docs/scripts.js
@@ -120,27 +120,27 @@ function changeCurrencyPrime() {
 
   if (currencySelectPrime.value == "Real") {
     currencyNew.innerHTML = "Real"
-    currencyYes.src = "/java-script/./assets-conversor/real.png/"
+    currencyYes.src = "./docs/assets-conversor/real.png"
   }
 
   if (currencySelectPrime.value == "dolar") {
     currencyNew.innerHTML = "Dólar americano"
-    currencyYes.src = "/java-script/./assets-conversor/dolar.png/"
+    currencyYes.src = "./docs/assets-conversor/dolar.png"
   }
 
   if (currencySelectPrime.value == "Euro") {
     currencyNew.innerHTML = "Euro"
-    currencyYes.src = "/java-script/./assets-conversor/logo-euro.png/"
+    currencyYes.src = "./docs/assets-conversor/logo-euro.png"
   }
 
   if (currencySelectPrime.value == "Libra") {
     currencyNew.innerHTML = "Libra"
-    currencyYes.src = "/java-script/./assets-conversor/libra 1.png/"
+    currencyYes.src = "./docs/assets-conversor/libra 1.png"
   }
 
   if (currencySelectPrime.value == "Bitcoin") {
     currencyNew.innerHTML = "Bitcoin"
-    currencyYes.src = "/java-script/./assets-conversor/bitcoin 1.png/"
+    currencyYes.src = "./docs/assets-conversor/bitcoin 1.png"
   }
 
 
@@ -163,13 +163,13 @@ function changeCurrency() {
 
   if (currencySelect.value == "dolar") {
     currencyName.innerHTML = "Dólar americano"
-    currencyImage.src = "./assets-conversor/dolar.png"
+    currencyImage.src = "./docs/assets-conversor/dolar.png"
   }
 
 
   if (currencySelect.value == "Real") {
     currencyName.innerHTML = "Real"
-    currencyImage.src = "./assets-conversor/real.png"
+    currencyImage.src = "./docs/assets-conversor/real.png"
 
 
 
@@ -177,19 +177,19 @@ function changeCurrency() {
 
   if (currencySelect.value == "Euro") {
     currencyName.innerHTML = "Euro"
-    currencyImage.src = "./assets-conversor/logo-euro.png"
+    currencyImage.src = "./docs/assets-conversor/logo-euro.png"
 
   }
 
   if (currencySelect.value == "Libra") {
     currencyName.innerHTML = "Libra"
-    currencyImage.src = "./assets-conversor/libra 1.png"
+    currencyImage.src = "./docs/assets-conversor/libra 1.png"
 
   }
 
   if (currencySelect.value == "Bitcoin") {
     currencyName.innerHTML = "Bitcoin"
-    currencyImage.src = "./assets-conversor/bitcoin 1.png"
+    currencyImage.src = "./docs/assets-conversor/bitcoin 1.png"
 
   }
 
